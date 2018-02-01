db2e454f135b6a5a04d7f782a873beb6d5f73a95
an_wch <an_wch@suixingpay>
2018-02-01 17:00:40 +0800

    fixbug webpack build remove test code

--- front/config/webpack.config.prod.js
+++ front/config/webpack.config.prod.js
@@ -106,132 +106,154 @@ module.exports = {
   },
   module: {
     strictExportPresence: true,
-    rules: [
-      // TODO: Disable require.ensure as it's not a standard language feature.
-      // We are waiting for https://github.com/facebookincubator/create-react-app/issues/2176.
-      // { parser: { requireEnsure: false } },
+      rules: [
+          // TODO: Disable require.ensure as it's not a standard language feature.
+          // We are waiting for https://github.com/facebookincubator/create-react-app/issues/2176.
+          // { parser: { requireEnsure: false } },
 
-      // First, run the linter.
-      // It's important to do this before Babel processes the JS.
-      // {
-      //   test: /\.(js|jsx|mjs)$/,
-      //   enforce: 'pre',
-      //   use: [
-      //     {
-      //       options: {
-      //         formatter: eslintFormatter,
-      //         eslintPath: require.resolve('eslint'),
-      //
-      //       },
-      //       loader: require.resolve('eslint-loader'),
-      //     },
-      //   ],
-      //   include: paths.appSrc,
-      // },
-      {
-        // "oneOf" will traverse all following loaders until one will
-        // match the requirements. When no loader matches it will fall
-        // back to the "file" loader at the end of the loader list.
-        oneOf: [
-          // "url" loader works just like "file" loader but it also embeds
-          // assets smaller than specified size as data URLs to avoid requests.
+          // First, run the linter.
+          // It's important to do this before Babel processes the JS.
+          // {
+          //   test: /\.(js|jsx|mjs)$/,
+          //   enforce: 'pre',
+          //   use: [
+          //     {
+          //       options: {
+          //         formatter: eslintFormatter,
+          //         eslintPath: require.resolve('eslint'),
+          //
+          //       },
+          //       loader: require.resolve('eslint-loader'),
+          //     },
+          //   ],
+          //   include: paths.appSrc,
+          // },
           {
-            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-            loader: require.resolve('url-loader'),
-            options: {
-              limit: 10000,
-              name: 'static/media/[name].[hash:8].[ext]',
-            },
-          },
-          // Process JS with Babel.
-          {
-            test: /\.(js|jsx|mjs)$/,
-            include: paths.appSrc,
-            loader: require.resolve('babel-loader'),
-            options: {
-              
-              compact: true,
-            },
-          },
-          // The notation here is somewhat confusing.
-          // "postcss" loader applies autoprefixer to our CSS.
-          // "css" loader resolves paths in CSS and adds assets as dependencies.
-          // "style" loader normally turns CSS into JS modules injecting <style>,
-          // but unlike in development configuration, we do something different.
-          // `ExtractTextPlugin` first applies the "postcss" and "css" loaders
-          // (second argument), then grabs the result CSS and puts it into a
-          // separate file in our build process. This way we actually ship
-          // a single CSS file in production instead of JS code injecting <style>
-          // tags. If you use code splitting, however, any async bundles will still
-          // use the "style" loader inside the async code so CSS from them won't be
-          // in the main CSS file.
-          {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract(
-              Object.assign(
-                {
-                  fallback: {
-                    loader: require.resolve('style-loader'),
-                    options: {
-                      hmr: false,
-                    },
+              // "oneOf" will traverse all following loaders until one will
+              // match the requirements. When no loader matches it will fall
+              // back to the "file" loader at the end of the loader list.
+              oneOf: [
+                  // "url" loader works like "file" loader except that it embeds assets
+                  // smaller than specified limit in bytes as data URLs to avoid requests.
+                  // A missing `test` is equivalent to a match.
+                  {
+                      test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                      loader: require.resolve('url-loader'),
+                      options: {
+                          limit: 10000,
+                          name: 'static/media/[name].[hash:8].[ext]',
+                      },
                   },
-                  use: [
-                    {
-                      loader: require.resolve('css-loader'),
+                  // Process JS with Babel.
+                  {
+                      test: /\.(js|jsx|mjs)$/,
+                      include: paths.appSrc,
+                      loader: require.resolve('babel-loader'),
                       options: {
-                        importLoaders: 1,
-                        minimize: true,
-                        sourceMap: shouldUseSourceMap,
+                          plugins: [
+                              "transform-decorators-legacy",
+                              ['import', {libraryName: 'antd', style: true}],
+                          ],
+                          // This is a feature of `babel-loader` for webpack (not Babel itself).
+                          // It enables caching results in ./node_modules/.cache/babel-loader/
+                          // directory for faster rebuilds.
+                          cacheDirectory: true,
                       },
-                    },
-                    {
-                      loader: require.resolve('postcss-loader'),
+                  },
+                  {
+                      test: /\.(svg)$/i,
+                      loader: 'svg-sprite-loader',
+                      include: [
+                          require.resolve('antd').replace(/warn\.js$/, ''),  //  antd-mobile内置svg
+                          // path.resolve(__dirname, '../src/myAppSvg'),            // 使用自定义svg
+                      ]
+                  },
+                  {
+                      test: /\.less$/,
+                      use: [
+                          require.resolve('style-loader'),
+                          require.resolve('css-loader'),
+                          {
+                              loader: require.resolve('postcss-loader'),
+                              options: {
+                                  ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+                                  plugins: () => [
+                                      autoprefixer({
+                                          browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4'],
+                                      }),
+                                      require('postcss-pxtorem')({ rootValue: 100, propWhiteList: [] })
+                                  ],
+                              },
+                          },
+                          {
+                              loader: require.resolve('less-loader'),
+                              options: {
+                                  modifyVars: { "@primary-color": "#1DA57A" },
+                              },
+                          },
+                      ],
+                  },
+                  // "postcss" loader applies autoprefixer to our CSS.
+                  // "css" loader resolves paths in CSS and adds assets as dependencies.
+                  // "style" loader turns CSS into JS modules that inject <style> tags.
+                  // In production, we use a plugin to extract that CSS to a file, but
+                  // in development "style" loader enables hot editing of CSS.
+                  {
+                      test: /\.css$/,
+                      use: [
+                          require.resolve('style-loader'),
+                          {
+                              loader: require.resolve('css-loader'),
+                              options: {
+                                  importLoaders: 1,
+                              },
+                          },
+                          {
+                              loader: require.resolve('postcss-loader'),
+                              options: {
+                                  // Necessary for external CSS imports to work
+                                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                                  ident: 'postcss',
+                                  plugins: () => [
+                                      require('postcss-flexbugs-fixes'),
+                                      autoprefixer({
+                                          browsers: [
+                                              '>1%',
+                                              'last 4 versions',
+                                              'Firefox ESR',
+                                              'not ie < 9', // React doesn't support IE8 anyway
+                                          ],
+                                          flexbox: 'no-2009',
+                                      }),
+                                  ],
+                              },
+                          },
+                      ],
+                  },
+                  // "file" loader makes sure those assets get served by WebpackDevServer.
+                  // When you `import` an asset, you get its (virtual) filename.
+                  // In production, they would get copied to the `build` folder.
+                  // This loader doesn't use a "test" so it will catch all modules
+                  // that fall through the other loaders.
+                  {
+                      // Exclude `js` files to keep "css" loader working as it injects
+                      // it's runtime that would otherwise processed through "file" loader.
+                      // Also exclude `html` and `json` extensions so they get processed
+                      // by webpacks internal loaders.
+                      exclude: [/\.js$/, /\.html$/, /\.json$/,
+                          /\.less$/,   //新增项
+                          /\.svg$/,    //新增项
+                      ],
+                      loader: require.resolve('file-loader'),
                       options: {
-                        // Necessary for external CSS imports to work
-                        // https://github.com/facebookincubator/create-react-app/issues/2677
-                        ident: 'postcss',
-                        plugins: () => [
-                          require('postcss-flexbugs-fixes'),
-                          autoprefixer({
-                            browsers: [
-                              '>1%',
-                              'last 4 versions',
-                              'Firefox ESR',
-                              'not ie < 9', // React doesn't support IE8 anyway
-                            ],
-                            flexbox: 'no-2009',
-                          }),
-                        ],
+                          name: 'static/media/[name].[hash:8].[ext]',
                       },
-                    },
-                  ],
-                },
-                extractTextPluginOptions
-              )
-            ),
-            // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
-          },
-          // "file" loader makes sure assets end up in the `build` folder.
-          // When you `import` an asset, you get its filename.
-          // This loader doesn't use a "test" so it will catch all modules
-          // that fall through the other loaders.
-          {
-            loader: require.resolve('file-loader'),
-            // Exclude `js` files to keep "css" loader working as it injects
-            // it's runtime that would otherwise processed through "file" loader.
-            // Also exclude `html` and `json` extensions so they get processed
-            // by webpacks internal loaders.
-            exclude: [/\.js$/, /\.html$/, /\.json$/],
-            options: {
-              name: 'static/media/[name].[hash:8].[ext]',
-            },
+                  },
+              ],
           },
           // ** STOP ** Are you adding a new loader?
           // Make sure to add the new loader(s) before the "file" loader.
-        ],
-      },
-    ],
+      ],
   },
   plugins: [
     // Makes some environment variables available in index.html.
